e69c2976ad351237285c9a77aa9cad290b08e6e6
Saurabh Pandey <saurabhpandey1344@gmail.com>
2026-06-27 11:57:45 +0530

    fix: add missing default export to Loader component

---
+++ src/components/Common/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-8 h-8 border-3 border-orange-200 border-t-orange-500 rounded-full animate-spin" />
+    </div>
+  );
+}
