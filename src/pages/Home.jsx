698c25e31d1674f2c011823e3c33cf7fa3e96bfa
Saurabh Pandey <saurabhpandey1344@gmail.com>
2026-07-24 12:52:39 +0530

    Remove duplicate Navbar and Footer from Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,15 @@
-import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import BookingSection from "../components/Booking/BookingSection";
 import PopularRoutes from "../components/PopularRoutes/PopularRoutes";
 import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
-import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-[#F8FAFC]">
-      <Navbar />
+    <div className="min-h-screen bg-[#F8FAFC]">
       <Hero />
       <BookingSection />
       <PopularRoutes />
       <WhyChooseUs />
-      <Footer />
     </div>
   );
 }
